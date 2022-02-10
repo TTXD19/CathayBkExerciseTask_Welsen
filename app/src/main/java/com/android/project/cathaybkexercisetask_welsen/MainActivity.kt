236997db7150337db016dba131f5c001d1d0389b
welsen <welsen9595@gmail.com>
2022-02-10 19:32:25 +0800

    feat: Add initial fragment transaction for main activity

--- app/src/main/java/com/android/project/cathaybkexercisetask_welsen/MainActivity.kt
+++ app/src/main/java/com/android/project/cathaybkexercisetask_welsen/MainActivity.kt
@@ -2,10 +2,28 @@ package com.android.project.cathaybkexercisetask_welsen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import com.android.project.cathaybkexercisetask_welsen.databinding.ActivityMainBinding
+import com.android.project.cathaybkexercisetask_welsen.ui.user_list.UserListFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+            .apply { setContentView(root) }
+
+        if (savedInstanceState == null) showUserListFragment()
+    }
+
+    private fun showUserListFragment() {
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            add<UserListFragment>(R.id.fl_main)
+        }
     }
 }
--- app/src/main/java/com/android/project/cathaybkexercisetask_welsen/ui/user_list/UserListFragment.kt
+++ app/src/main/java/com/android/project/cathaybkexercisetask_welsen/ui/user_list/UserListFragment.kt
@@ -5,27 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.android.project.cathaybkexercisetask_welsen.R
+import com.android.project.cathaybkexercisetask_welsen.databinding.FragmentUserListBinding
 
 class UserListFragment : Fragment() {
 
+    private lateinit var binding: FragmentUserListBinding
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_list, container, false)
-    }
+    ): View = FragmentUserListBinding.inflate(layoutInflater, container, false)
+        .also { binding = it }
+        .root
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-    }
 
-    companion object {
-        fun newInstance(param1: String, param2: String) =
-            UserListFragment().apply {
-                arguments = Bundle().apply {}
-            }
     }
 }
